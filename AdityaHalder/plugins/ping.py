c91c086ae56cf55e9883af8d1c968d98e0a4208c
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-11-25 10:16:16 +0530

    Update ping.py

--- AdityaHalder/plugins/ping.py
+++ AdityaHalder/plugins/ping.py
@@ -5,16 +5,16 @@ from datetime import datetime
 @app.on_message(commandx(["ping"]) & SUDOERS)
 async def alive_check(client, message):
     start = datetime.now()
-    m = await eor(message, "**🤖 Pong !**")
+    m = await eor(message, "**🩸𝗣𝗢𝗡𝗚 𝗕𝗔𝗕𝗬 !**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await m.edit(f"**🤖 Pinged !\nLatency:** `{ms}` ms")
+    await m.edit(f"**🩸𝗣𝗜𝗡𝗚𝗘𝗗 !\nLatency:** `{ms}` ms")
 
 
 __NAME__ = "Ping"
 __MENU__ = f"""
-**🥀 Check Userbot Server
-Ping Latency ✨...**
+**🥀 𝗖𝗛𝗘𝗖𝗞 𝗨𝗦𝗘𝗥𝗕𝗢𝗧 
+𝗣𝗜𝗡𝗚 𝗟𝗔𝗧𝗘𝗡𝗖𝗬 ✨...**
 
 **Example:** `.ping`
 """
